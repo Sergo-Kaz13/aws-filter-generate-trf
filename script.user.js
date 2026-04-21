7b7837d21dac6f8dfd53ec36541a1148fda8ab7b
Serhii Kazmirchuk <71965782+Sergo-Kaz13@users.noreply.github.com>
2026-04-21 18:08:09 +0200

    Update version to 1.0.1

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AWS Filter Generate TRF
 // @namespace    https://github.com/Sergo-Kaz13/aws-filter-generate-trf
-// @version      1.0.0
+// @version      1.0.1
 // @description  Collects products from one site, filters by warehouse location availability, returns filtered list
 // @author       Sergo-Kaz13
 // @require      file:///D:/Programming/Projects/aws-filter-generate-trf/script.user.js
