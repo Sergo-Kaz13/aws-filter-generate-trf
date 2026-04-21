db9f9d022fbea725fa8bfcda53714855a3022aff
Serhii Kazmirchuk <71965782+Sergo-Kaz13@users.noreply.github.com>
2026-04-21 18:11:30 +0200

    Update match

--- script.user.js
+++ script.user.js
@@ -5,8 +5,8 @@
 // @description  Collects products from one site, filters by warehouse location availability, returns filtered list
 // @author       Sergo-Kaz13
 // @require      file:///D:/Programming/Projects/aws-filter-generate-trf/script.user.js
-// @match        http://127.0.0.1:5500/*
-// @match        http://127.0.0.1:5501/*
+// @match        https://aws.autodoc.de/store/transfer/waiting*
+// @match        https://m13.autodoc.de/admin-panel/operational-info/stock-shelves*
 // @grant        GM_setValue
 // @grant        GM_getValue
 // @updateURL    https://raw.githubusercontent.com/Sergo-Kaz13/aws-filter-generate-trf/master/script.user.js
