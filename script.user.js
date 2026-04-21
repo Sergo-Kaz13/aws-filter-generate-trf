23ed21de83306ac18ca0fdcfa893dab172fa7143
Serhii Kazmirchuk <71965782+Sergo-Kaz13@users.noreply.github.com>
2026-04-21 18:22:57 +0200

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -1,10 +1,9 @@
 // ==UserScript==
 // @name         AWS Filter Generate TRF
 // @namespace    https://github.com/Sergo-Kaz13/aws-filter-generate-trf
-// @version      1.0.1
-// @description  Collects products from one site, filters by warehouse location availability, returns filtered list
+// @version      1.0.2
+// @description  Collects products from one site...
 // @author       Sergo-Kaz13
-// @require      file:///D:/Programming/Projects/aws-filter-generate-trf/script.user.js
 // @match        https://aws.autodoc.de/store/transfer/waiting*
 // @match        https://m13.autodoc.de/admin-panel/operational-info/stock-shelves*
 // @grant        GM_setValue
